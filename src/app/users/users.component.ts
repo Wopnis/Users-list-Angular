c39074699b11d3e5edf889ad85747196f58fb8dc
Konstantin800306 <wopnis2016@outlook.com>
2019-12-09 14:54:46 +0300

    first commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { USERS } from '../usersMock';
+import {User} from '../user';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+
+
+export class UsersComponent implements OnInit {
+  user = User;
+  users = USERS;
+  selectedUser: User;
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/usersMock.ts
@@ -0,0 +1,13 @@
+import {User} from './user';
+
+export const USERS: User[] = [
+  {firstName: 'Ivan', lastName: 'Ivanov', age: 22, city: 'Moscow' },
+  {firstName: 'Elena', lastName: 'Sidorova', age: 28, city: 'Minsk' },
+  {firstName: 'Peter', lastName: 'Petrov', age: 45, city: 'Moscow' },
+  {firstName: 'Jack', lastName: 'Nicholson', age: 74, city: 'NewYork'},
+  {firstName: 'John', lastName: 'Johnson', age: 35, city: 'London' },
+  {firstName: 'Jack', lastName: 'Caste', age: 34, city: 'Paris' },
+  {firstName: 'Igor', lastName: 'Egorov', age: 50, city: 'Moscow' },
+  {firstName: 'Semen', lastName: 'Semenov', age: 25, city: 'Minsk' }
+];
+
